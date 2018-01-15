8c991b950011d284299999fa858fc89f08c1d2b0
espejor <espejor@gmail.com>
2018-01-15 10:59:43 +0100

    Actualización Actualización versión estructura de paquetes del curso Java adaits. 2017-2018

--- src/adaits/cursoJava/colegio/GrupoProfesional.java
+++
@@ -1,5 +0,0 @@
-package adaits.cursoJava.colegio;
-
-public enum GrupoProfesional {
-
-}
--- src/adaits/cursoJava/colegio/Mantenimiento.java
+++
@@ -1,9 +0,0 @@
-package adaits.cursoJava.colegio;
-
-import adaits.cursoJava.Persona.Dni;
-
-public class Mantenimiento extends PersonalAuxiliar {
-    public Mantenimiento(Dni dni, String name, String apellido1, String apellido2) {
-        super(dni, name, apellido1, apellido2);
-    }
-}
